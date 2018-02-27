09492a868675c7980981b8af6bb5c7e614534cc7
castrolol <lu4n.k4str0@gmail.com>
2018-02-27 14:28:05 -0400

    Sugestão de propriedades e erro em mailto:
    para uma melhor leitura, é mais interessante usarmos o descontructor
    para o uso de propriedades de um objeto, como em profile, é mais interessante
    o uso de `const {firstName} = this.profile` no inicio do render, ao uso de
    `<p>{this.profile.firstName}</p>` no corpo do jsx, para evitar uma leitura
    rebuscada.
    o "mailto:{this.profile.email}" não iria fazer o que deveria ser feito,
    já que a interpolação em typescript é feita com backticks (`) e com ($)
    como `mailto:${mail}` ! :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,61 @@
-import * as React from 'react';
-import './App.css';
-import './profile.css';
-import Profile from './Profile';
+import * as React from "react";
+import "./App.css";
+import "./profile.css";
+import Profile from "./Profile";
 
 class App extends React.Component {
-
   profile = new Profile();
 
   render() {
+    const {
+      top,
+      picture,
+      friends,
+      photos,
+      firstName,
+      lastName,
+      title,
+      email,
+      phone,
+      instagram,
+      age,
+      bio
+    } = this.profile;
+
+    const linkMailTo = `mailto:${email}`;
 
     return (
       <div className="App">
         <div className="profile-body">
-          <div className="profile-top"><img src={this.profile.top} /></div>
+          <div className="profile-top">
+            <img src={top} />
+          </div>
           <div className="profile-left">
-            <img src={this.profile.picture} className="profile-picture" />
-            <p className="profile-data-title">{this.profile.friends}</p>
+            <img src={picture} className="profile-picture" />
+            <p className="profile-data-title">{friends}</p>
             <p className="profile-data-text">Friends</p>
-            <p className="profile-data-title">{this.profile.photos}</p>
+            <p className="profile-data-title">{photos}</p>
             <p className="profile-data-text">Photos</p>
           </div>
           <div className="profile-right">
-            <h1>{this.profile.firstName} <strong>{this.profile.lastName}</strong></h1>
-            <h2>{this.profile.title}</h2>
-            <p className="profile-data-text black"><i className="fa fa-phone" /> {this.profile.phone}</p>
+            <h1>
+              {firstName} <strong>{lastName}</strong>
+            </h1>
+            <h2>{title}</h2>
             <p className="profile-data-text black">
-              <i className="fa fa-envelope" /> <a href="mailto:{this.profile.email}">{this.profile.email}</a>
+              <i className="fa fa-phone" /> {phone}
             </p>
-            <p className="profile-data-text black"><i className="fa fa-instagram" /> {this.profile.instagram}</p>
-            <p className="profile-data-text black"><i className="fa fa-calendar" /> {this.profile.age} anos</p>
+            <p className="profile-data-text black">
+              <i className="fa fa-envelope" /> <a href={linkMailTo}>{email}</a>
+            </p>
+            <p className="profile-data-text black">
+              <i className="fa fa-instagram" /> {instagram}
+            </p>
+            <p className="profile-data-text black">
+              <i className="fa fa-calendar" /> {age} anos
+            </p>
           </div>
-          <div className="profile-bio">{this.profile.bio}</div>
+          <div className="profile-bio">{bio}</div>
         </div>
       </div>
     );
