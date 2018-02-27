3422f910627b99dc0c4f07bed3be5f5b3a966726
castrolol <lu4n.k4str0@gmail.com>
2018-02-27 14:25:06 -0400

    Sugestão de nomenclatura para classes e atributos
    As classes em typescript são escritas com letra maiscula pascal case,
    esse padrão é o mais utilizado (quase que unanime), porem os atributos
    são escritos com letra minuscula (o da pra ver aqui https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines#names)

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,32 @@ import Profile from './Profile';
 
 class App extends React.Component {
 
-  Profile = new Profile;
+  profile = new Profile();
 
   render() {
 
     return (
       <div className="App">
         <div className="profile-body">
-          <div className="ProfileTop"><img src={this.Profile.Top} /></div>
+          <div className="profile-top"><img src={this.profile.top} /></div>
           <div className="profile-left">
-            <img src={this.Profile.Picture} className="profile-picture" />
-            <p className="profile-data-title">{this.Profile.Friends}</p>
+            <img src={this.profile.picture} className="profile-picture" />
+            <p className="profile-data-title">{this.profile.friends}</p>
             <p className="profile-data-text">Friends</p>
-            <p className="profile-data-title">{this.Profile.Photos}</p>
+            <p className="profile-data-title">{this.profile.photos}</p>
             <p className="profile-data-text">Photos</p>
           </div>
           <div className="profile-right">
-            <h1>{this.Profile.FirstName} <strong>{this.Profile.LastName}</strong></h1>
-            <h2>{this.Profile.Title}</h2>
-            <p className="profile-data-text black"><i className="fa fa-phone" /> {this.Profile.Phone}</p>
+            <h1>{this.profile.firstName} <strong>{this.profile.lastName}</strong></h1>
+            <h2>{this.profile.title}</h2>
+            <p className="profile-data-text black"><i className="fa fa-phone" /> {this.profile.phone}</p>
             <p className="profile-data-text black">
-              <i className="fa fa-envelope" /> <a href="mailto:{this.Profile.Email}">{this.Profile.Email}</a>
+              <i className="fa fa-envelope" /> <a href="mailto:{this.profile.email}">{this.profile.email}</a>
             </p>
-            <p className="profile-data-text black"><i className="fa fa-instagram" /> {this.Profile.Instagram}</p>
-            <p className="profile-data-text black"><i className="fa fa-calendar" /> {this.Profile.Age} anos</p>
+            <p className="profile-data-text black"><i className="fa fa-instagram" /> {this.profile.instagram}</p>
+            <p className="profile-data-text black"><i className="fa fa-calendar" /> {this.profile.age} anos</p>
           </div>
-          <div className="profile-bio">{this.Profile.Bio}</div>
+          <div className="profile-bio">{this.profile.bio}</div>
         </div>
       </div>
     );
--- src/Profile.ts
+++ src/Profile.ts
@@ -1,23 +1,23 @@
 
 class Profile  {
 
-    Picture = require('./profile-picture.jpg');
-    Top = require('./profile-top.jpg');
+    picture = require('./profile-picture.jpg');
+    top = require('./profile-top.jpg');
 
-    Friends = 569;
-    Photos = '1,324';
+    friends = 569;
+    photos = '1,324';
     
-    FirstName = 'Guilherme';
-    LastName = 'Castro';
-    Title = 'CEO, My React Course';
+    firstName = 'Guilherme';
+    lastName = 'Castro';
+    title = 'CEO, My React Course';
     
-    Phone = '(65) 99633-1136';
-    Email = 'guicastro@gmail.com';
-    Instagram = 'guiga_castro';
-    Age = 32;
+    phone = '(65) 99633-1136';
+    email = 'guicastro@gmail.com';
+    instagram = 'guiga_castro';
+    age = 32;
 
     // tslint:disable-next-line:max-line-length
-    Bio = 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere';
+    bio = 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere';
 }
 
 export default Profile;
